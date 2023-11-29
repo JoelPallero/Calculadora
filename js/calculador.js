248f5f55a5ed559488202f9bb89e426c41dba687
Joel Pallero <40552100+JoelPallero@users.noreply.github.com>
2023-11-29 15:50:05 -0300

    Update Funcion de signos

--- js/calculador.js
+++ js/calculador.js
@@ -16,7 +16,7 @@ function operador(queHago){
 // num[0] es el primer numero y num[1] es el segundo.
 const acumular = function(agregar){
     if (!operando) {
-        if (num[0] == null) {
+        if (num[0] == undefined) {
             num[0] = agregar;
             num[3] = agregar;
         }
@@ -53,51 +53,90 @@ const acumular = function(agregar){
 }
 
 //num[2] es el donde va el signo de la operacion
-const operacionSigno = function(signoElegido){
-    if(operando == true && num[2] != undefined && num[1] != undefined){
-        num[2] = signoElegido;
-        num[0] = num[4];
-        num[3] = num[0] + ' ' + num[2] + ' ';
-        document.getElementById('acumulador').value = num[3];
-        num[1] = undefined;
-    }else{
-        let caracter = num[3].charAt(num[3].length-1)
-        if(caracter == ' ' && signoElegido != '∛' && signoElegido != '√'){
+const operacionSigno = (signoElegido) => {
+    /* CONDICIONEALES PARA ASIGNACION O CAMBIO DE SIGNO */
+
+    // 1 Asignar el signo elegido POR PRIMERA VEZ: 1 Si el signo es cualquiera de las raices o 2 si es cualquiera de los otros signos
+    /*
+        condiciones
+        Es raiz? & 
+            si -> resultado == null
+                    si -> Registrar signo
+                          Esperar numA para dar resultado.
+                    no -> numA = resultado
+                          realizar raiz al resultado
+                          mostrar el resutlado
+            no -> 3    
+    */
+    operando = true;
+    //Verificacion para saber si el signo es una raiz
+    if(signoElegido == '√' || signoElegido == '∛'){
+        if(num[4] == undefined){
             num[2] = signoElegido;
-            num[3] = num[0] + ' ' + num[2] + ' ';
-            document.getElementById('acumulador').value = num[3];
-        }else{
-            /* La operacion no entra en este if */
-            if(num[4] != undefined && signoElegido == '√' || signoElegido == '∛'){
+            num[3] = signoElegido;
+        }
+        else{
+            num[1] = parseFloat(document.getElementById('resultado').value);
+            num[2] = signoElegido;
+            num[3] = signoElegido + parseFloat(document.getElementById('resultado').value);
+            operacion();
+        }
+        document.getElementById('acumulador').value = num[3];
+    }
+    else{
+        if(num[4] != undefined || num[4] != null){
+            num[0] = parseFloat(document.getElementById('resultado').value);
+            num[2] = signoElegido;
+        }
+        else{
+            if(num[1] != undefined || num[1] != null){
+                operacion();
+                num[0] = parseFloat(document.getElementById('resultado').value);
                 num[2] = signoElegido;
-                num[3] = num[2] + num[1];
-                document.getElementById('acumulador').value = num[3];
-                document.getElementById('resultado').value = '';
-                console.log(num[1] + ' ' + num[2])
-            }else{
-                if(signoElegido == '√' || signoElegido == '∛'){
-                    num[2] = signoElegido;
-                    num[3] = num[2];
-                    operando = true;
-                    document.getElementById('acumulador').value = num[3];
-                    console.log(num[1] + ' ' + num[2])
-                }
-                else{
-                    if(num[1] == undefined && num[0] != undefined){
-                        num[2] = signoElegido;
-                        num[3] += ' ' + num[2] + ' ';
-                
-                        operando = true;
-                        coma = false;
-                        document.getElementById('acumulador').value = num[3];
-                    }
-                    else{
-                        document.getElementById('acumulador').value = 'ERROR';
-                    }
-                }
+            }
+            else{
+                num[2] = signoElegido;                
             }
         }
+        num[3] = num[0] + ' ' + signoElegido + ' ';
     }
+
+
+    // 2 Reemplazar un signo elegido, mientras que la última actividad dada haya sido un signo.
+    /*
+        condiciones
+        signoElegido seleccionado? & numB == null?
+        si -> 4
+        no -> numB != null?
+                realizar operacion
+                resultado se asigna a numA
+                signoElegido nuevo
+                numB == null
+    */
+
+    // 3 Asignar por segunda vez un signo cuando el resultado esté dado: Esto haria que el resultado se asigne como el primer dato al momento de apretar el signo.
+    /*
+        condiciones
+        resultado dado?
+        -- si-> resultado se asigna a numA
+                signo es == raiz?
+                si -> realizar operacion. radicando = resultado
+                   mostrar resultado
+                no -> 4
+        -- no -> 2 -> no
+    */
+
+        // -------
+    
+
+    
+   // 4 Flujo normal de eleccion del signo
+   /*
+   condiciones
+        numA != null? && signo != raiz
+        si -> signo = signoElegido
+        no -> no hacer nada   
+   */
 }
 
 //Se ejecuta con el igual, '=', para ya realizar la operacion final
